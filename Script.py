37aca37bb0a9d6623608afe029b0429e12f2e5ef
bossjk00 <sample2sing@gmail.com>
2025-02-02 22:43:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/jk_movie_adda">main chhnle</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name} {title}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/jk_movie_adda">main chhnle</a>」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
